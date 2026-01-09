d56e82a5355bd3c1328bc19f04e7ac3b5c1c7140
Nguyễn Trọng Nam <maxsvippro@gmail.com>
2026-01-09 22:26:56 +0700

    DeadZone

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player Hit Dead Zone");
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Die();
+            }
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -63,6 +63,7 @@ public class Player : MonoBehaviour
         HandleAnimation();
     }
 
+    public void Die() => Destroy(gameObject);
     public void Knockback()
     {
         if (isKnockbacked)
